5fdc31272fefe2b9c1a63c58f0ebc5234f16b78d
Shubhm Jain <jshubham2304@gmail.com>
2019-01-29 21:32:08 +0530

    lambda

--- Kotlin/src/demo.kt
+++ Kotlin/src/demo.kt
@@ -1,10 +1,23 @@
+import java.util.*
+
 fun main(args: Array<String>) {
-    var fortune: String
-    for (i in 1..10) {
-        fortune = getFortune()
-        println("\nYour fortune is: $fortune")
-        if (fortune.contains("Take it easy")) break
+//    var fortune: String
+//    for (i in 1..10) {
+//        fortune = getFortune()
+//        println("\nYour fortune is: $fortune")
+//        if (fortune.contains("Take it easy")) break
+//    }
+    var rollDice0 = { sides: Int ->
+        if (sides == 0) 0
+        else Random().nextInt(sides) + 1
     }
+
+    var rollDice2: (Int) -> Int = { sides ->
+        if (sides == 0) 0
+        else Random().nextInt(sides) + 1
+    }
+
+    gameplay(rollDice2(0))
 }
 
 fun getFortune() : String {
@@ -17,5 +30,11 @@ fun getFortune() : String {
             "Treasure your friends, because they are your greatest fortune.")
     print("\nEnter your birthday: ")
     val birthday = readLine()?.toIntOrNull() ?: 1
+
     return fortunes[birthday.rem(fortunes.size)]
+
 }
+
+fun gameplay(roll : Int) {
+   print("$roll")
+    }
